d0ba170cfd5416c0a573d2275742033842da82d2
anshul19912 <anshulsingh19912@gmail.com>
2022-11-20 03:46:23 +0530

    final commit

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import '../widgets/cart_Item.dart';
 import 'cart.dart';
 
 class OrderItem {
